a182200a1375eb6eddcdfea606eacb90fdf8aab1
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-04-13 16:14:19 +1000

    changed defualt position to match Humble

--- launch/turtlebot3_world.launch.py
+++ launch/turtlebot3_world.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='-2.0')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='-0.5')
 
     world = os.path.join(
